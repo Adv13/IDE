d2234303d94d659c5bc5c236a399f306d5569420
Adv <tiffanydpro923@gmail.com>
2023-09-22 20:38:49 +0200

    initial commit

---
+++ src/components/CodeTab.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CodeTab() {
+  return (
+    <textarea spellCheck="false" className='bg-zinc-900 text-slate-50 text-xl p-8 block h-full w-full focus:outline-none resize-none'></textarea>
+  )
+}
---
+++ src/features/tabs.js
@@ -0,0 +1,34 @@
+import {html, css, jss} from "../assets/index"
+
+const initialState = [
+    {
+        id:1,
+        lang:"html",
+        imgURL: html,
+        buttonContent: "HTML",
+        code: `<div>
+        <h1>Editeur de code avec React</h1>
+        <p>Codez directement sur votre navigateur.</p>
+        </div>`
+    }
+    {
+        id:1,
+        lang:"html",
+        imgURL: html,
+        buttonContent: "HTML",
+        code: `<div>
+        <h1>Editeur de code avec React</h1>
+        <p>Codez directement sur votre navigateur.</p>
+        </div>`
+    }
+    {
+        id:1,
+        lang:"html",
+        imgURL: html,
+        buttonContent: "HTML",
+        code: `<div>
+        <h1>Editeur de code avec React</h1>
+        <p>Codez directement sur votre navigateur.</p>
+        </div>`
+    }
+]
