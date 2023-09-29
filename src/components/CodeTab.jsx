4b509e69e76f429a08b8af88933df36e2c7df4fb
Adv <tiffanydpro923@gmail.com>
2023-09-29 15:17:00 +0200

    maj project

--- src/components/CodeTab.jsx
+++ src/components/CodeTab.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { updateCode } from '../features/tabs'
+import { useDispatch } from 'react-redux'
 
-export default function CodeTab() {
+export default function CodeTab({code, id}) {
+  const dispatch = useDispatch();
+
+
   return (
-    <textarea spellCheck="false" className='bg-zinc-900 text-slate-50 text-xl p-8 block h-full w-full focus:outline-none resize-none'></textarea>
+    <textarea 
+    onChange={e => dispatch(updateCode({id, value: e.target.value}))}
+    value={code}
+    spellCheck="false" className='bg-zinc-900 text-slate-50 text-xl p-8 block h-full w-full focus:outline-none resize-none'></textarea>
   )
 }
--- src/features/tabs.js
+++ src/features/tabs.js
@@ -1,4 +1,6 @@
-import {html, css, jss} from "../assets/index"
+import {html, css, js} from "../assets/index"
+import {createSlice} from "@reduxjs/toolkit"
+
 
 const initialState = [
     {
@@ -9,26 +11,38 @@ const initialState = [
         code: `<div>
         <h1>Editeur de code avec React</h1>
         <p>Codez directement sur votre navigateur.</p>
-        </div>`
-    }
+</div>`
+    },
     {
-        id:1,
-        lang:"html",
-        imgURL: html,
-        buttonContent: "HTML",
-        code: `<div>
-        <h1>Editeur de code avec React</h1>
-        <p>Codez directement sur votre navigateur.</p>
-        </div>`
-    }
+        id:2,
+        lang:"css",
+        imgURL: css,
+        buttonContent: "CSS",
+        code: `bod{
+            font-family:Roboto, sans-serif;
+            padding:25px;
+            color:#111;
+            background-color:#f1f1f1;
+}`
+    },
     {
-        id:1,
-        lang:"html",
-        imgURL: html,
-        buttonContent: "HTML",
-        code: `<div>
-        <h1>Editeur de code avec React</h1>
-        <p>Codez directement sur votre navigateur.</p>
-        </div>`
+        id:3,
+        lang:"js",
+        imgURL: js,
+        buttonContent: "JavaScript",
+        code: `console.log('Hello World);`
     }
 ]
+
+export const codeUpdater = createSlice({
+    name:'code-updater',
+    initialState,
+    reducers:{
+        updateCode:(state, action) => {
+            state.find(obj => obj.id ===  action.payload.id).code = action.payload.value
+        }
+    }
+})
+
+export const {updateCode} = codeUpdater.actions;
+export default codeUpdater.reducer;
